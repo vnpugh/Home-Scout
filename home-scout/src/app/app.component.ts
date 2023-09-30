8133d2aa1223ccf0a48206a1da91c689e9a86815
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2023-09-29 21:27:16 -0500

    Initial Commit

--- home-scout/src/app/app.component.ts
+++ home-scout/src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  template: `<h1>Hello world!</h1>`,
 })
 export class AppComponent {
   title = 'home-scout';
